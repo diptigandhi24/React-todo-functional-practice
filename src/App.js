629d38c3d1a16942392e8de2e6ae4118c8403210
diptigandhi24 <gandhidipti.s@gmail.com>
2019-07-17 12:26:51 +0530

    separated the form component along with its onclick and state feature

--- src/App.js
+++ src/App.js
@@ -5,28 +5,11 @@ import "./App.css";
 
 function App() {
   let [list, addToList] = useState([]);
-  let [text, setText] = useState("");
-  const onSubmitHandler = event => {
-    event.preventDefault();
-    if (!text.length) return;
-    let newItem = {
-      text: text,
-      id: Date.now()
-    };
 
-    addToList(list.concat(newItem));
-    setText((text = ""));
-  };
   return (
     <div>
-      <Form
-        onSubmitHandler={onSubmitHandler}
-        onChange={e => {
-          setText((text = e.target.value));
-        }}
-        value={text}
-      />
       <Todo list={list} />
+      <Form addToList={addToList} list={list} />
     </div>
   );
 }
